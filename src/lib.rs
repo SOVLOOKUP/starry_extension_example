61bf7690d593af34c6b4d94089870493705e3d4b
Starry North <805408477@qq.com>
2022-05-27 15:40:12 +0800

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use starry_extension_interface::{Extension,EmitSender, ListenSender};
+use std::{thread::sleep, time::Duration};
+use chrono::prelude::*;
+extern crate chrono;
+
+use starry_extension_interface::{
+    EmitContent, EmitSender, Extension, ListenContent, ListenSender, ListenType,
+};
 
 #[no_mangle]
 pub fn new() -> Box<dyn Extension> {
@@ -16,15 +22,34 @@ impl Extension for ExtensionSayHello {
         "{}"
     }
 
-    fn load(
-        &self,
-        _emit_sender: &EmitSender,
-        _listen_sender: &ListenSender,
-    ) {
-        todo!()
+    fn load(&self, emit_sender: &EmitSender, listen_sender: &ListenSender) {
+        let emit_sender2 = emit_sender.clone();
+        // 监听 plus 事件
+        listen_sender.send(ListenContent {
+            event: "plus".to_string(),
+            content_type: ListenType::Listen(Box::new(move |x| {
+                let mut y = String::from(x.payload().unwrap());
+                y.push_str("plustr");
+                emit_sender2.clone().send(EmitContent {
+                    id: x.id().to_string(),
+                    event: "callback".to_string(),
+                    payload: y,
+                }).unwrap();
+            })),
+        }).unwrap();
+
+        loop {
+            // 每两秒一次心跳
+            emit_sender.send(EmitContent {
+                id: "".to_string(),
+                event: "heartbeat".to_string(),
+                payload: Local::now().to_string(),
+            }).unwrap();
+            sleep(Duration::from_secs(2))
+        }
     }
 
     fn unload(&self) {
         println!("[{}] Unload instance!", self.id());
-    }    
+    }
 }
