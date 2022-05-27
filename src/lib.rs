e2828941e5ebf32ac072b41151081957b905c287
Starry North <805408477@qq.com>
2022-05-27 21:43:23 +0800

    555放弃

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ use chrono::prelude::*;
 extern crate chrono;
 
 use starry_extension_interface::{
-    EmitContent, EmitSender, Extension, ListenContent, ListenSender, ListenType,
+    EmitContent, ArcEmitSender, Extension, ListenContent, ArcListenSender, ListenType,
 };
 
 #[no_mangle]
-pub fn new() -> Box<dyn Extension> {
+pub fn new() -> Box<dyn Extension + Send> {
     Box::new(ExtensionSayHello {})
 }
 
@@ -22,31 +22,31 @@ impl Extension for ExtensionSayHello {
         "{}"
     }
 
-    fn load(&self, emit_sender: &EmitSender, listen_sender: &ListenSender) {
-        let emit_sender2 = emit_sender.clone();
-        // 监听 plus 事件
-        listen_sender.send(ListenContent {
-            event: "plus".to_string(),
-            content_type: ListenType::Listen(Box::new(move |x| {
-                let mut y = String::from(x.payload().unwrap());
-                y.push_str("plustr");
-                emit_sender2.clone().send(EmitContent {
-                    id: x.id().to_string(),
-                    event: "callback".to_string(),
-                    payload: y,
-                }).unwrap();
-            })),
-        }).unwrap();
+    fn load(&self, emit_sender: &ArcEmitSender, listen_sender: &ArcListenSender) {
+        // let emit_sender2 = emit_sender.clone();
+        // // 监听 plus 事件
+        // listen_sender.send(ListenContent {
+        //     event: "plus".to_string(),
+        //     content_type: ListenType::Listen(Box::new(move |x| {
+        //         let mut y = String::from(x.payload().unwrap());
+        //         y.push_str("plustr");
+        //         emit_sender2.clone().send(EmitContent {
+        //             id: x.id().to_string(),
+        //             event: "callback".to_string(),
+        //             payload: y,
+        //         }).unwrap();
+        //     })),
+        // }).unwrap();
 
-        loop {
-            // 每两秒一次心跳
+        // loop {
+        //     // 每两秒一次心跳
             emit_sender.send(EmitContent {
                 id: "".to_string(),
                 event: "heartbeat".to_string(),
                 payload: Local::now().to_string(),
             }).unwrap();
-            sleep(Duration::from_secs(2))
-        }
+        //     sleep(Duration::from_secs(2))
+        // }
     }
 
     fn unload(&self) {
